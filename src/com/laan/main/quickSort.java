c522ea72ed0b9ae4a214ee96f8c7729c0ac90071
Нурбол Сембаев <231199@astanait.edu.kz>
2024-05-10 15:39:09 +0500

    g

--- src/com/laan/main/quickSort.java
+++ src/com/laan/main/quickSort.java
@@ -1,27 +1,45 @@
-public class Practice(args) {
-public static void
-int[l arr -
-mergeSort(arr) ;
-for(int = O;
-< art'. length; 1++)
-        System.ovt.print(arrßl +
-        private static void arr) {
-    int n arr.tength;
-    if(n I) return;
-    int mid = n / 2
-    into 1 -
-            - new int[midl;
-    int[] r
-            = new int[n - mid];
-    for(int = 0; i < mid; i++)
-= arr[i];
-    for(int = mid; i < n; i")
-            - midl - arr[il;
-    123,
-            55,
-            3,
-            1000};
-mergeSort(t) ;
-mergeSort(r) ;
-merge (arr. I,
-       r);
+package com.laan.quickSort;
+
+public class quickSort {
+
+    public static void sort(int[] array, int low, int high) {
+        if (array.length == 0 || low >= high)
+            return;
+        int pivot = array[low + (high - low) / 2];
+        int i = low, j = high;
+        while (i <= j) {
+            while (array[i] < pivot) {
+                i++;
+            }
+            while (array[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        if (low < j) sort(array, low, j);
+        if (high > i) sort(array, i, high);
+    }
+
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {12, 11, 13, 5, 6, 7};
+
+        // Call the sort method with the correct parameters
+        quickSort.sort(arr, 0, arr.length - 1);
+
+        System.out.println("Sorted array is");
+        printArray(arr);
+    }
+}
