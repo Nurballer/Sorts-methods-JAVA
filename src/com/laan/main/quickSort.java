54fbc19e63221e138711064cb5eaa60fcb40f7d0
Нурбол Сембаев <231199@astanait.edu.kz>
2024-05-10 15:36:57 +0500

    g

---
+++ src/com/laan/main/quickSort.java
@@ -0,0 +1,27 @@
+public class Practice(args) {
+public static void
+int[l arr -
+mergeSort(arr) ;
+for(int = O;
+< art'. length; 1++)
+        System.ovt.print(arrßl +
+        private static void arr) {
+    int n arr.tength;
+    if(n I) return;
+    int mid = n / 2
+    into 1 -
+            - new int[midl;
+    int[] r
+            = new int[n - mid];
+    for(int = 0; i < mid; i++)
+= arr[i];
+    for(int = mid; i < n; i")
+            - midl - arr[il;
+    123,
+            55,
+            3,
+            1000};
+mergeSort(t) ;
+mergeSort(r) ;
+merge (arr. I,
+       r);
