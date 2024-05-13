51360f826fd8bb8dad00ced8927f090cb1f271da
Нурбол Сембаев <231199@astanait.edu.kz>
2024-05-13 11:59:14 +0500

    g

--- src/com/laan/main/selectionSort.java
+++ src/com/laan/main/selectionSort.java
@@ -1,4 +1,29 @@
 package com.laan.main;
 
 public class selectionSort {
+    private static void selectSort(int[] arr) {
+        int n = arr.length;
+        for(int i = 0; i < n; i++) {
+            int min_index = i;
+            for(int j = i + 1; j < n; j++) {
+                if(arr[min_index] > arr[j])
+                    min_index = j;
+            }
+            swap(i, min_index, arr);
+        }
+    }
+
+    private static void swap(int i, int min_index, int[] arr) {
+        int temp = arr[i];
+        arr[i] = arr[min_index];
+        arr[min_index] = temp;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = {1000, 5, 7, 3, 2, 500, 13, 19};
+        selectSort(arr);
+
+        for(int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+    }
 }
